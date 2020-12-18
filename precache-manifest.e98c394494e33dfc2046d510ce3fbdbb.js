dc3446350c9d0674c9d2e79551d1472e6e30226b
Hossein <hossein.tb@gmail.com>
2020-12-18 03:16:20 -0500

    Updates

---
+++ precache-manifest.e98c394494e33dfc2046d510ce3fbdbb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2251293635e5e1ee80b1bb8d1676dcaf",
+    "url": "/redux-photo-gallery/index.html"
+  },
+  {
+    "revision": "65620d62c743ec4a77a8",
+    "url": "/redux-photo-gallery/static/css/main.761a1650.chunk.css"
+  },
+  {
+    "revision": "d3af34c4e269e483c83b",
+    "url": "/redux-photo-gallery/static/js/2.cba3a883.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/redux-photo-gallery/static/js/2.cba3a883.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65620d62c743ec4a77a8",
+    "url": "/redux-photo-gallery/static/js/main.2e42e8ec.chunk.js"
+  },
+  {
+    "revision": "d44de031d33275bde32b",
+    "url": "/redux-photo-gallery/static/js/runtime-main.3546e064.js"
+  }
+]);
